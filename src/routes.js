ec39c6fa8bf7ae9e4f71311f7510a272bd43db3d
sserges <agounkeserge@gmail.com>
2019-04-18 17:46:56 +0000

    to move in the url with React Router

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Router, Route, Link, browserHistory, IndexRoute } from "react-router";
+
+import PostList from "./containers/post-list";
+import PostForm from "./containers/post-form";
+
+export class Routes extends Component {
+  render() {
+    return (
+      <div>
+        <Router history={browserHistory}>
+          <Route path="/" component={PostList} />
+          <Route path="create-post" component={PostForm} />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default Routes;
