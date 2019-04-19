eda87fb7a12224ac9d38075079d5953b6eb8146f
sserges <agounkeserge@gmail.com>
2019-04-19 17:50:04 +0000

    form validation

--- src/containers/post-form.js
+++ src/containers/post-form.js
@@ -9,36 +9,57 @@ import { createPost } from "../actions/index";
 
 const formConfig = {
   form: "createPostForm",
-  fields: ["title", "content", "author"]
+  fields: ["title", "content", "author"],
+  validate: validate
 };
 
 class PostForm extends Component {
   render() {
-    const { fields, handleSubmit } = this.props;
-    console.log(fields);
+    const {
+      fields: { title, content, author },
+      handleSubmit,
+      errors
+    } = this.props;
+    console.log(errors);
     return (
       <div>
         <h1>Nouveau Post</h1>
         <form onSubmit={handleSubmit(this.createPost.bind(this))}>
-          <div className="form-group">
+          <div
+            className={`form-group ${
+              title.touched && title.invalid ? "has-danger" : ""
+            }`}
+          >
             <label>Titre</label>
-            <input className="form-control" type="text" {...fields.title} />
-            <div />
+            <input className="form-control" type="text" {...title} />
+            <div>{title.touched && errors.title}</div>
           </div>
-          <div className="form-group">
+          <div
+            className={`form-group ${
+              content.touched && content.invalid ? "has-danger" : ""
+            }`}
+          >
             <label>Description</label>
-            <textarea className="form-control" {...fields.content} />
-            <div />
+            <textarea className="form-control" {...content} />
+            <div>{content.touched && errors.content}</div>
           </div>
-          <div className="form-group">
+          <div
+            className={`form-group ${
+              author.touched && author.invalid ? "has-danger" : ""
+            }`}
+          >
             <label>Auteur</label>
-            <input className="form-control" type="text" {...fields.author} />
-            <div />
+            <input className="form-control" type="text" {...author} />
+            <div>{author.touched && errors.author}</div>
           </div>
           <Link to={"/"} className="button_space">
             <button className="btn btn-danger">Retour</button>
           </Link>
-          <button type="submit" className="btn btn-primary">
+          <button
+            disabled={this.props.invalid}
+            type="submit"
+            className="btn btn-primary"
+          >
             Créer
           </button>
         </form>
@@ -50,6 +71,21 @@ class PostForm extends Component {
     this.props.createPost(post);
     browserHistory.push("/");
   }
+}
+
+function validate(values) {
+  const errors = {};
+  if (!values.title) {
+    errors.title = "Veuillez remplir le titre";
+  }
+  if (!values.content) {
+    errors.content = "Veuillez remplir la description";
+  }
+  if (!values.author) {
+    errors.author = "Veuillez remplir l'auteur";
+  }
+
+  return errors;
 }
 
 const mapDispatchToProps = dispatch => ({
