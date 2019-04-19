ebf75b66803e282da4740916692a60f7c7f8f93f
sserges <agounkeserge@gmail.com>
2019-04-19 16:22:32 +0000

    use of redux form

--- src/containers/post-form.js
+++ src/containers/post-form.js
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
 import { Link } from "react-router";
+import { reduxForm } from "redux-form";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+
+import { createPost } from "../actions/index";
+
+const formConfig = {
+  form: "createPostForm",
+  fields: ["title", "content", "author"]
+};
 
 class PostForm extends Component {
   render() {
+    const { fields } = this.props;
+    console.log(fields);
     return (
       <div>
         <h1>Nouveau Post</h1>
         <form>
           <div className="form-group">
             <label>Titre</label>
-            <input className="form-control" type="text" />
+            <input className="form-control" type="text" {...fields.title} />
             <div />
           </div>
           <div className="form-group">
             <label>Description</label>
-            <textarea className="form-control" />
+            <textarea className="form-control" {...fields.content} />
             <div />
           </div>
           <div className="form-group">
             <label>Auteur</label>
-            <input className="form-control" type="text" />
+            <input className="form-control" type="text" {...fields.author} />
             <div />
           </div>
           <Link to={"/"} className="button_space">
@@ -34,4 +46,7 @@ class PostForm extends Component {
   }
 }
 
-export default PostForm;
+export default connect(
+  null,
+  null
+)(reduxForm(formConfig)(PostForm));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from "redux";
+import { reducer as ReducerForm } from "redux-form";
 
 import ReducerPosts from "./reducer-posts";
 import ReducerActivePost from "./reducer-active-post";
 
 const rootReducer = combineReducers({
   posts: ReducerPosts,
-  activePost: ReducerActivePost
+  activePost: ReducerActivePost,
+  form: ReducerForm
 });
 
 export default rootReducer;
